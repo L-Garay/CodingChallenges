850cca5d3cdd0576caf8bda2ee11bd8e87532901
Logan Garay <garay.logan@gmail.com>
2020-02-24 10:50:06 -0700

    initial setup and first 3 challenges

--- README.md
+++ README.md
@@ -9,13 +9,15 @@ link to thier site is https://edabit.com/
 ### This project will be split into three sections
 
 ```
-The green section will be challenges I was able to complete without any resources.  The yellow section will be challenges that I had to use outside resources to solve but was able to solve.  Finally, the red section will be for those challenges that even after using other resources, I wasn't at the time able to solve.
+The green section will be challenges I was able to complete.
+The yellow section will be code I used to solve the challenge.
+Finally, the red section will be for those challenges that even after using other resources, I wasn't at the time able to solve.
 ```
 
-### I will have the answers stored in another view
+### This file is just to track my progress through challenges
 
 ```
-The answer numbers will match the challenge numbers
+This isn't meant to show myself completeling the hardest and craziet challenges, but to get my mind going in the morning and practice whiteboard-type questions
 ```
 
 <!-- ### Customize configuration
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <ol class="challenges mx-1">
+          <li>
+            <p>(2/24/2020 || Javascript)</p>
+            <p>Write a function that returns true if all characters in a string are identical and false otherwise.</p>
+            <img src="../assets/solutions/1.png" alt />
+          </li>
+          <li>
+            <p>(2/24/2020 || Javascript)</p>
+            <p>Create a function that accepts an array and returns the last item in the array.</p>
+            <img src="../assets/solutions/second.png" alt />
+          </li>
+          <li>
+            <p>(2/24/2020 || Javascript)</p>
+            <p>Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.</p>
+            <img src="../assets/solutions/third.png" alt />
+          </li>
+        </ol>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home"
+};
 </script>
+
+<style>
+.challenges {
+  display: flex;
+  flex-direction: row;
+  height: 90vh;
+  overflow-y: scroll;
+  width: 100vw;
+  flex-wrap: wrap;
+}
+ol li {
+  margin: 10pt;
+  padding: 3pt;
+  border: 1pt solid black;
+}
+</style>
